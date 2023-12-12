e5ba065c7929eee05da6fee36c3569dc0075bbd0
nacimchafaa <118357726+nacimchafaa@users.noreply.github.com>
2023-12-12 12:06:21 +0100

    some improvements to grasp

--- grasp.jl
+++ grasp.jl
@@ -2,21 +2,52 @@ using Random
 using Dates
 include("parser.jl")
 
-function grasp(terminals::Matrix{Float32}, lv1Concentrators::Matrix{Float32}, lv2Concentrators::Matrix{Float32}, C::Int64,
+# we will use several occurences of GRASP to generate individuals of our population
+function grasp(n::Int64, m::Int64, nLevel1::Int64, nLevel2::Int64, terminals::Matrix{Float32}, lv1Concentrators::Matrix{Float32}, lv2Concentrators::Matrix{Float32}, C::Int64,
     distanceTerminalConcentrators::Matrix{Int64}, distanceConcentrators::Matrix{Int64}, Lv2Costs::Vector{Int64})
 
     # sorting terminals by descending distance from each lv1 concentrator
-    sortedTerminalsLevel1::Matrix{Int64} = Matrix{Int64}(undef,(length(terminals),length(lv1Concentrators)))
-    for j in 1:length(lv1Concentrators)
-        sortedTerminalsLevel1[:,i] = sort(terminals,by= term -> distanceTerminalConcentrators[term,j],rev=true)
-    end
+    sortedTerminalsLevel1 = zeros(Int64, n, nLevel1)
+    for i in 1:n
+        sortedTerminalsLevel1[i,:] = sort(distanceTerminalConcentrators[:,i],rev=true)
+    end  
     
-    return sortedTerminalsLevel1
+    # sorting lv1 concentrators by descending distance from each lv2 concentrator
+    sortedLv1Lv2Concentrators = zeros(Int64, nLevel1, nLevel2) 
+    for i in 1:nLevel1
+        sortedLv1Lv2Concentrators[i,:] = sort(distanceConcentrators[i,:],rev=true)
+    end
+
+    # we calculate the sum of the entering arcs for each level 1 concentrators
+    potentials = zeros(Int64, nLevel1)
+    for i in 1:nLevel1
+        potentials[i] = sum(sortedTerminalsLevel1[i,1:numberSelectedLevel1])
+    end
+    println("potentials: ", potentials)
+
+    # we randomly add the first level 1 concentrator to the solution
+    α1 = 0.7
+    randomLevel1 = rand(1:nLevel1)
+    println("randomLevel1: ", randomLevel1)
+    selectedLv1Concentrators::Vector{Int64} = [randomLevel1]
+    println("selectedLv1Concentrators: ", selectedLv1Concentrators)
+    remainingLv1Concentrators::Vector{Int64} = []
+    println("remainingLv1Concentrators: ", remainingLv1Concentrators)
+    for i in 1:nLevel1
+        append!(remainingLv1Concentrators, i)
+    end
+    deleteat!(remainingLv1Concentrators, randomLevel1)
+
+    #now, we launch the GRASP until we get this number of level 1 concentrators, for the first objective
+    # we add the remaining concentrators with a probability of α1
+    
 end
+
+
 
 m, n, nLevel1, nLevel2, lv1Concentrators, lv2Concentrators, terminals = loadInstance("data/small1.txt")
 distanceConcentrators = distancesConcentrators(lv1Concentrators, lv2Concentrators)
 distanceTerminalConcentrators = distancesTerminalsConcentrators(lv1Concentrators, terminals)
 Lv2Costs = rand(minimum(distanceConcentrators):maximum(distanceConcentrators),nLevel2)
 C = floor(Int, 2/3 * nLevel1)
-sorted = grasp(terminals, lv1Concentrators, lv2Concentrators, C, distanceTerminalConcentrators, distanceConcentrators, Lv2Costs)
+grasp(n, m, nLevel1, nLevel2, terminals, lv1Concentrators, lv2Concentrators, C, distanceTerminalConcentrators, distanceConcentrators, Lv2Costs)
