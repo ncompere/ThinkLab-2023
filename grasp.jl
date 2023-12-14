5ec59aa52754a2db95cfe32944ac216bf59eb569
nacimchafaa <118357726+nacimchafaa@users.noreply.github.com>
2023-12-14 11:19:25 +0100

    Working GRASP

--- grasp.jl
+++ grasp.jl
@@ -1,13 +1,14 @@
 using Random
 using Dates
 include("parser.jl")
+include("functions.jl")
 
 # we will use several occurences of GRASP to generate individuals of our population
 function grasp(n::Int64, m::Int64, nLevel1::Int64, nLevel2::Int64, terminals::Matrix{Float32}, lv1Concentrators::Matrix{Float32}, lv2Concentrators::Matrix{Float32}, C::Int64,
     distanceTerminalConcentrators::Matrix{Int64}, distanceConcentrators::Matrix{Int64}, Lv2Costs::Vector{Int64})
     
     valueZ1 = 0
-    vaueZ2 = 0
+    valueZ2 = 0
 
     # sorting terminals by descending distance from each lv1 concentrator
     sortedTerminalsLevel1 = zeros(Int64, n, nLevel1)
@@ -35,29 +36,24 @@ function grasp(n::Int64, m::Int64, nLevel1::Int64, nLevel2::Int64, terminals::Ma
         end
     end
 
-    println("C: $C")
     # we randomly add the first level 1 concentrator to the solution
     α1 = 0.7
     randomLevel1 = rand(1:nLevel1)
-    println("randomLevel1: ", randomLevel1)
     selectedLv1Concentrators::Vector{Int64} = [randomLevel1]
     remainingLv1Concentrators::Vector{Int64} = []
     for i in 1:nLevel1
         append!(remainingLv1Concentrators, i)
     end
     deleteat!(remainingLv1Concentrators, randomLevel1)
-    println("selectedLv1Concentrators: ", selectedLv1Concentrators)
-    println("remainingLv1Concentrators: ", remainingLv1Concentrators)
 
     #now, we launch the GRASP until we get this number of level 1 concentrators, for the first objective
     # we add the remaining level1 concentrators with a probability of α1
         # we look for the best and worst potentials
     for c1 in 2:C
         bestC1 = potentials[remainingLv1Concentrators[1]]
-        println("bestC1: ", bestC1)
         worstC1 = bestC1
+
         for i in eachindex(remainingLv1Concentrators)
-            if i > 1
                 currentPotential = potentials[remainingLv1Concentrators[i]]
                 if currentPotential < bestC1
                     bestC1 = currentPotential
@@ -65,31 +61,32 @@ function grasp(n::Int64, m::Int64, nLevel1::Int64, nLevel2::Int64, terminals::Ma
                 if currentPotential > worstC1
                     worstC1 = currentPotential
                 end
-            end
         end
-        threshold = worstC1 - α1*(worstC1 - bestC1)
 
+        threshold = worstC1 - α1*(worstC1 - bestC1)
         RCL = []
+
         for i in eachindex(remainingLv1Concentrators)
             candidate = remainingLv1Concentrators[i]
             if potentials[candidate] <= threshold
                 append!(RCL, candidate)
             end
         end
+
         newConcentrator = RCL[rand(1:size(RCL,1))]
         append!(selectedLv1Concentrators, newConcentrator)
         deleteat!(remainingLv1Concentrators, findall(x->x==newConcentrator,remainingLv1Concentrators))
     end
-    println("selectedLv1Concentrators: ", selectedLv1Concentrators)
+
     # now, we select which arcs are selected between terminals and level 1 concentrators
     # the second GRASP starts with a randomly chosen link
     α2 = 0.7
     linksTerminalLevel1 = zeros(Int64,n) # we initialize the links
     randTerminal = rand(1:n) # we choose a random terminal
-    randLevel1 = selectedLv1Concentrators[rand(1:C)] # we choose a random level 1 concentrator
+    randLevel1 = selectedLv1Concentrators[rand(1:length(selectedLv1Concentrators))] # we choose a random level 1 concentrator
     linksTerminalLevel1[randTerminal] = randLevel1  # we add the link to the solution
     valueZ1 += distanceTerminalConcentrators[randLevel1, randTerminal] # we add the cost of the link to the solution
-
+    valueZ2 += distanceTerminalConcentrators[randLevel1, randTerminal] # we save the distance of the link to use it in the second objective
     remainingTerminals = []
     for i in 1:n
         append!(remainingTerminals, i)
@@ -100,11 +97,11 @@ function grasp(n::Int64, m::Int64, nLevel1::Int64, nLevel2::Int64, terminals::Ma
 
     # we add the remaining links with a probability of α2
     for k in 2:n
-        bestLink1 = distanceTerminalConcentrators[remainingLv1Concentrators[1],remainingTerminals[1]]
+        bestLink1 = distanceTerminalConcentrators[selectedLv1Concentrators[1],remainingTerminals[1]]
         worstLink1 = bestLink1
-        for i in eachindex(remainingLv1Concentrators)
+        for i in eachindex(selectedLv1Concentrators)
             for j in eachindex(remainingTerminals)
-                costCandidate = distanceTerminalConcentrators[remainingLv1Concentrators[i],remainingTerminals[j]]
+                costCandidate = distanceTerminalConcentrators[selectedLv1Concentrators[i],remainingTerminals[j]]
                 if costCandidate < bestLink1
                     bestLink1 = costCandidate
                 end
@@ -116,37 +113,43 @@ function grasp(n::Int64, m::Int64, nLevel1::Int64, nLevel2::Int64, terminals::Ma
 
         threshold = worstLink1 - α2*(worstLink1 - bestLink1)
         empty!(RCL)
-
-        for i in eachindex(remainingLv1Concentrators)
+        for i in eachindex(selectedLv1Concentrators)
             for j in eachindex(remainingTerminals)
-                cost = distanceTerminalConcentrators[remainingLv1Concentrators[i],remainingTerminals[j]]
+                cost = distanceTerminalConcentrators[selectedLv1Concentrators[i],remainingTerminals[j]]
                 if cost <= threshold
-                    push!(RCL, [remainingLv1Concentrators[i],remainingTerminals[j]])
+                    push!(RCL, [selectedLv1Concentrators[i],remainingTerminals[j]])
                 end
             end
         end
+    
+        newArc = RCL[rand(1:size(RCL,1))]  
+        linksTerminalLevel1[newArc[2]] = newArc[1]
+        deleteat!(remainingTerminals, findall(x->x==newArc[2],remainingTerminals))
 
-        newLink = RCL[rand(1:size(RCL,1))]  
-        #selectedConcentratorLv1 = [newLink[1]]
-        linksTerminalLevel1[newLink[2]] = newLink[1]
-        deleteat!(remainingTerminals, findall(x->x==newLink[2],remainingTerminals))
-        valueZ1 += distanceTerminalConcentrators[newLink[1], newLink[2]]
+        valueZ1 += distanceTerminalConcentrators[newArc[1], newArc[2]]
 
+        if valueZ2 < distanceTerminalConcentrators[newArc[1], newArc[2]]
+            valueZ2 = distanceTerminalConcentrators[newArc[1], newArc[2]]
+        end
     end
 
     # now that we have the set of linked terminals and level 1 concentrators, we determine the set of level 2 concentrators
     α3 = 0.7
     selectedLv2Concentrators = []
     remainingLv2Concentrators = []
+
+    randLevel2 = rand(1:nLevel2)
+    append!(selectedLv2Concentrators, randLevel2)
+
     for i in 1:nLevel2
         append!(remainingLv2Concentrators, i)
     end
+    deleteat!(remainingLv2Concentrators, randLevel2)
 
-    for i in 1:nLevel2
+    for i in 2:nLevel2
         bestC2 = potentials[remainingLv2Concentrators[1]]
         worstC2 = bestC2
         for j in eachindex(remainingLv2Concentrators)
-            if j > 1
                 currentPotential = potentials[remainingLv2Concentrators[j]]
                 if currentPotential < bestC2
                     bestC2 = currentPotential
@@ -154,7 +157,6 @@ function grasp(n::Int64, m::Int64, nLevel1::Int64, nLevel2::Int64, terminals::Ma
                 if currentPotential > worstC2
                     worstC2 = currentPotential
                 end
-            end
         end
         threshold = worstC2 - α3*(worstC2 - bestC2)
         RCL = []
@@ -170,8 +172,6 @@ function grasp(n::Int64, m::Int64, nLevel1::Int64, nLevel2::Int64, terminals::Ma
         end
         deleteat!(remainingLv2Concentrators, findall(x->x==newLv2Concentrator,remainingLv2Concentrators))
     end
-    println("selectedLv2Concentrators: ", selectedLv2Concentrators)
-    println("remainingLv2Concentrators: ", remainingLv2Concentrators)
     # add the costs of the level 2 concentrators to valueZ1
     for i in eachindex(selectedLv2Concentrators)
         valueZ1 += Lv2Costs[i]
@@ -209,16 +209,14 @@ function grasp(n::Int64, m::Int64, nLevel1::Int64, nLevel2::Int64, terminals::Ma
                     push!(RCL, j)
                 end
             end
-            newLink = RCL[rand(1:size(RCL,1))]
-            linksLv1Lv2Concentrators[level1] = remainingLevel2Links[newLink]
-            println("linksLv1Lv2Concentrators: ", linksLv1Lv2Concentrators)
-            valueZ1 += distanceConcentrators[level1, remainingLevel2Links[newLink]]
+            newArc = RCL[rand(1:size(RCL,1))]
+            linksLv1Lv2Concentrators[level1] = remainingLevel2Links[newArc]
+            valueZ1 += distanceConcentrators[level1, remainingLevel2Links[newArc]]
         end
     end
-    println("selectedLevel1Concentrators: ", selectedLv1Concentrators)
-    allConcentrators = vcat(selectedLv1Concentrators, selectedLv2Concentrators)
-    println("all working Concentrators: ", allConcentrators)
-    println("links Lv 1Lv2 Concentrators: ", linksLv1Lv2Concentrators)
+    #allConcentrators = vcat(selectedLv1Concentrators, selectedLv2Concentrators)
+
+    return solution(selectedLv1Concentrators, linksTerminalLevel1, selectedLv2Concentrators, linksLv1Lv2Concentrators, valueZ1, valueZ2)
 end
 
 
@@ -228,4 +226,11 @@ distanceConcentrators = distancesConcentrators(lv1Concentrators, lv2Concentrator
 distanceTerminalConcentrators = distancesTerminalsConcentrators(lv1Concentrators, terminals)
 Lv2Costs = rand(minimum(distanceConcentrators):maximum(distanceConcentrators),nLevel2)
 C = floor(Int, 2/3 * nLevel1)
-grasp(n, m, nLevel1, nLevel2, terminals, lv1Concentrators, lv2Concentrators, C, distanceTerminalConcentrators, distanceConcentrators, Lv2Costs)
+solGrasp = grasp(n, m, nLevel1, nLevel2, terminals, lv1Concentrators, lv2Concentrators, C, distanceTerminalConcentrators, distanceConcentrators, Lv2Costs)
+
+println("Selected level 1 concentrators: ", solGrasp.selectedLv1)
+println("Links between terminals and level 1 concentrators: ", solGrasp.linksTerminalLevel1)
+println("Selected level 2 concentrators: ", solGrasp.selectedLv2)
+println("Links between level 1 and level 2 concentrators: ", solGrasp.linksLevel1Level2)
+println("Value of the first objective: ", solGrasp.valueObj1)
+println("Value of the second objective: ", solGrasp.valueObj2)
