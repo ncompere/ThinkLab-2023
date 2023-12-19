3b45c53d113d424a834ab329c4663c6990ae82c8
Nicolas Compère <75181320+ncompere@users.noreply.github.com>
2023-12-19 15:57:50 +0100

    Update plot.jl

--- plot.jl
+++ plot.jl
@@ -4,15 +4,13 @@ using Plots
 include("parser.jl")
 include("functions.jl")
 
-function plot_instance(fname::String)
-    m, n, nLevel1, nLevel2, lv1Concentrators, lv2Concentrators, terminals = loadInstance(fname)
-
+function plot_instance_init(data::instance)
     plot(legend=:outerbottom) # to start with an empty plot
-    plot!(scatter!(lv1Concentrators[:,1], lv1Concentrators[:,2], label="level 1 concentrators", markershape=:diamond, color="blue"))
-    plot!(scatter!(lv2Concentrators[:,1], lv2Concentrators[:,2], label="level 2 concentrators", markershape=:rect, color="red"))
-    plot!(scatter!(terminals[:,1], terminals[:,2], label="terminals", color="black"))
-
+    plot!(scatter!(data.lv1Concentrators[:,1], data.lv1Concentrators[:,2], label="level 1 concentrators", markershape=:diamond, color="blue"))
+    plot!(scatter!(data.lv2Concentrators[:,1], data.lv2Concentrators[:,2], label="level 2 concentrators", markershape=:rect, color="red"))
+    plot!(scatter!(data.terminals[:,1], data.terminals[:,2], label="terminals", color="black"))
 end
 
 # testing
-plot_instance("data/small1.txt")
+data = loadInstance("data/small1.txt")
+plot_instance_init(data)
