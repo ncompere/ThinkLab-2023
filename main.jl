06fbc4f5c854d07612568d3051b82d0bd76fb800
Nicolas Compère <75181320+ncompere@users.noreply.github.com>
2023-12-21 01:49:10 +0100

    Nice plots

--- main.jl
+++ main.jl
@@ -6,7 +6,7 @@ include("functions.jl")
 include("skipList.jl")
 include("tabu.jl")
 
-using Plots
+# using Plots
 
 function main()
     # chargement de l'instance
@@ -18,6 +18,9 @@ function main()
     # generattion d'une population initiale de solutions avec GRASP
     nbIterationsGRASP = 200
     solutionsInitiales::Vector{solution} = []
+    solutionsTabu::Vector{solution} = []
+    archiveGRASP = SkipList()
+    archiveTabu = SkipList()
     #archive = SkipList()
 
     # initialisation des vecteurs pour le plot des solutions
@@ -34,15 +37,20 @@ function main()
         push!(Z1, solGrasp.valueObj1)
         push!(Z2, solGrasp.valueObj2)
         solTabu = tabu(solGrasp, data)
+        push!(solutionsTabu, solTabu)
         push!(T1, solTabu.valueObj1)
         push!(T2, solTabu.valueObj2)
-        #addArchive(archive, [solGrasp.valueObj1, -solGrasp.valueObj2])
+        addArchive(archiveGRASP, [solGrasp.valueObj1, -solGrasp.valueObj2])
+        addArchive(archiveTabu, [solTabu.valueObj1, -solTabu.valueObj2])
     end
     timeGRASP = round(time()- getTime, digits=4)
     println("Temps de résolution : $timeGRASP s")
     #affichageSkiplist(archive)
     #println("Nombre de points non dominés : ", nbrPoint(archive))
     #points = setOfSolutions(archive)
+
+    pointsGRASP = setOfSolutions(archiveGRASP)
+    pointsTabu = setOfSolutions(archiveTabu)
 
     # solve avec MOA
     Y1, Y2 = solve_vOpt(vOptRes(data))
@@ -54,9 +62,40 @@ function main()
         push!(Z2, -(points[i][2]))
     end
 =#
-    plot(Y1,Y2,seriestype=:scatter, title="vOPT", xlabel="Y1", ylabel="Y2")
-    plot!(Z1,Z2,seriestype=:scatter, title="solutions GRASP", xlabel="Z1", ylabel="Z2")
-    plot!(T1,T2,seriestype=:scatter, title="solutions Tabu", xlabel="T1", ylabel="T2")
+    plot(Y1,Y2,seriestype=:scatter, title="Objective space", xlabel="Z1", ylabel="Z2", label="vOpt", size=(800,600))
+    plot!(Z1,Z2,seriestype=:scatter, label="GRASP")
+    display(plot!(T1,T2,seriestype=:scatter, label="Tabu"))
+    savefig("plot/Solution space.png")
+
+    # solution plot on a map
+    plot_instance_init(data)
+    plot_solution(solutionsInitiales[1], data, "GRASP solution")
+    plot_solution(solutionsTabu[1], data, "Tabu solution")
+
+    arc_Z1::Vector{Int64} = []
+    arc_Z2::Vector{Int64} = []
+    # plot taboo
+    arc_T1::Vector{Int64} = []
+    arc_T2::Vector{Int64} = []
+
+    for i in eachindex(pointsGRASP)
+        # add to Z1 the first index of each point in the set of solutions
+        push!(arc_Z1, pointsGRASP[i][1])
+        # add to Z2 the second index of each point in the set of solutions
+        push!(arc_Z2, -(pointsGRASP[i][2]))
+    end
+
+    for i in eachindex(pointsTabu)
+        # add to T1 the first index of each point in the set of solutions
+        push!(arc_T1, pointsTabu[i][1])
+        # add to T2 the second index of each point in the set of solutions
+        push!(arc_T2, -(pointsTabu[i][2]))
+    end
+
+    plot(Y1,Y2,seriestype=:scatter, title="Non dominated points", xlabel="Z1", ylabel="Z2", label="vOpt", size=(800,600))
+    plot!(arc_Z1,arc_Z2,seriestype=:scatter, label="GRASP")
+    display(plot!(arc_T1,arc_T2,seriestype=:scatter, label="Tabu"))
+    savefig("plot/Y_N.png")
 #=
 
     # initialization of the reference sets
--- plot.jl
+++ plot.jl
@@ -6,18 +6,20 @@ include("functions.jl")
 include("tabu.jl")
 
 function plot_instance_init(data::instance)
-    plot(legend=:outerbottom) # to start with an empty plot
+    plot(legend=:outerbottom, size=(800,800)) # to start with an empty plot
     plot!(scatter!(data.lv1Concentrators[:,1], data.lv1Concentrators[:,2], label="level 1 concentrators", markershape=:diamond, color="blue"))
     plot!(scatter!(data.lv2Concentrators[:,1], data.lv2Concentrators[:,2], label="level 2 concentrators", markershape=:rect, color="red"))
     plot!(scatter!(data.terminals[:,1], data.terminals[:,2], label="terminals", color="black"))
+    display(plot!(title="Initial map"))
+    savefig("plot/initial_map.png")
 end
 
 function plot_solver(sol::solution, data::instance)
-
+    
 end
 
-function plot_solution(sol::solution, data::instance)
-    plot(legend=:outerbottom) # to start with an empty plot
+function plot_solution(sol::solution, data::instance, title="Solution")
+    plot(legend=:outerbottom, size=(800,800)) # to start with an empty plot
     plot!(scatter!(data.lv1Concentrators[sol.selectedLv1,1], data.lv1Concentrators[sol.selectedLv1,2], label="level 1 concentrators", markershape=:diamond, color="blue"))
     plot!(scatter!(data.lv2Concentrators[sol.selectedLv2,1], data.lv2Concentrators[sol.selectedLv2,2], label="level 2 concentrators", markershape=:rect, color="red"))
     plot!(scatter!(data.terminals[:,1], data.terminals[:,2], label="terminals", color="black"))
@@ -32,7 +34,8 @@ function plot_solution(sol::solution, data::instance)
     for i in 1:length(sol.selectedLv1)
         plot!([data.lv1Concentrators[sol.selectedLv1[i],1], data.lv2Concentrators[sol.linksLevel1Level2[i],1]], [data.lv1Concentrators[sol.selectedLv1[i],2], data.lv2Concentrators[sol.linksLevel1Level2[i],2]], linewidth=1, color="grey", primary=false)
     end
-    display(plot!(title="Solution"))
+    display(plot!(title=title))
+    savefig("plot/$title.png")
 end
 
 # testing
@@ -40,12 +43,12 @@ end
 # println("Instance loaded")
 # sol = grasp(data)
 # println("GRASP done")
-# println("LV1 coordinates : ", data.lv1Concentrators)
-# println("LV2 coordinates : ", data.lv2Concentrators)
-# println("Terminals coordinates : ", data.terminals)
-# println("Solution : ", sol)
+# # # println("LV1 coordinates : ", data.lv1Concentrators)
+# # # println("LV2 coordinates : ", data.lv2Concentrators)
+# # # println("Terminals coordinates : ", data.terminals)
+# # # println("Solution : ", sol)
 # display(plot_instance_init(data))
-# plot_solution(sol, data)
+# plot_solution(sol, data, "GRASP solution")
 # sol = tabu(sol, data)
 # println("Tabu done")
-# display(plot_solution(sol, data))
+# plot_solution(sol, data, "Tabu solution")
